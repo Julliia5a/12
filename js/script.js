b317b0ef1e0d7cb5a4868ac9f2c9172fa96d636f
Julliia5a <korni010@umn.edu>
2015-05-15 15:36:59 -0500

    uploading web files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+$("#FP1").delay(1000).fadeIn("slow");
+
+$("#FP2").delay(3000).fadeIn("slow");
+
+$("#FP3").delay(5000).fadeIn("slow");
+
+$("#FP4").delay(7000).fadeIn("slow");
+
+});
+	// All of your jQuery and Javascript code goes in here.
+
